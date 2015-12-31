878aecaf16f297b9e2a0273f8527ffc2d2e2194c
Jeremy Laskar <jlaskar@disqus.com>
2015-12-30 19:46:57 -0800

    popup settings close, save, delete. sidebar editing & seeking.

--- public/src/js/controllers/MainCtrl.js
+++ public/src/js/controllers/MainCtrl.js
@@ -2,6 +2,8 @@ var MainCtrl = angular.module('MainCtrl', []);
 var player, mouseX, mouseY;
 var cardOpen = false;
 var sidebarState = 0;
+var currentX = 0;
+var currentY = 0;
 
 MainCtrl.directive('urlInput', function() {
 	return function($scope, $element){
@@ -28,7 +30,6 @@ MainCtrl.directive('urlInput', function() {
 						//'onStateChange': onPlayerStateChange
 					},
 						playerVars: {
-							controls: 0,
 							playsinline: 1,
 							showinfo: 0
 						}
@@ -53,6 +54,8 @@ MainCtrl.directive('isDraggable', function() {
 					} else {
 						$('.card-settings').removeClass('top').addClass('bottom');
 					}
+					currentX = (ui.offset.left/$(".player-container").width())*100;
+					currentY = (ui.offset.top/$(".player-container").height())*100;
 				}
 			});
 		}
@@ -65,10 +68,13 @@ function MainController($scope, $window) {
 
 	$scope.colorList = ['#7bd148','#5484ed','#a4bdfc','#46d6db','#7ae7bf','#51b749','#fbd75b','#ffb878','#ff887c','#dc2127','#dbadff','#e1e1e1', '#ffffff'];
 
-	$scope.emojiMessage = {};
-	$scope.selectedEmoji = '🐺';
-
+	$scope.currentText = '';
+	$scope.selectedEmoji = ':wolf:';
 	$scope.emojiBgColor = '#7bd148';
+	currentX = 0;
+	currentY = 0;
+
+	$scope.cardIndex = [];
 
 	$scope.resizeEvent = function() {
 		$('.player-container').css('height', document.body.clientWidth * 0.5625);
@@ -90,59 +96,58 @@ function MainController($scope, $window) {
 		mouseY = e.pageY;
 	};
 
-	$scope.expandSidebar = function() {
-		if (sidebarState === 0) {
-			$('.sidebar').addClass('small');
-			$('.sidebar .smaller').removeClass('disabled');
-			sidebarState++;
-		} else if (sidebarState == 1){
-			$('.sidebar').removeClass('small').addClass('large');
-			$('.sidebar .larger').addClass('disabled');
-			sidebarState++;
-		}
-	};
-
-	$scope.collapseSidebar = function() {
-		if (sidebarState == 1) {
-			$('.sidebar').removeClass('small');
-			$('.sidebar .smaller').addClass('disabled');
-			sidebarState--;
-		} else if (sidebarState == 2){
-			$('.sidebar').removeClass('large').addClass('small');
-			$('.sidebar .larger').removeClass('disabled');
-			sidebarState--;
-		}
-	};
-
-
 	$scope.pause = function() {
 		if ($scope.player.getPlayerState() == 1) {
 			$scope.player.pauseVideo();
-			$scope.expandSidebar();
-			var offset = $(".player-container").offset();
+			$scope.displayCard();
+		}
+	};
+
+	$scope.displayCard = function(card) {
+		if (card){
+			$scope.currentTime = card.time;
+			$scope.player.seekTo(card.time);
+			$scope.player.pauseVideo();
+			$scope.currentText = card.text;
+			$scope.selectedEmoji = card.emoji;
+			$scope.emojiBgColor = card.bg;
+
 			$('.card').removeClass('bounceOut').addClass('bounceIn');
 			$('.card').css({
 				'visibility': 'visible',
-				'top': ((mouseY - offset.top - 32)/$(".player-container").height())*100 + '%',
-				'left': ((mouseX - 32)/$(".player-container").width())*100 + '%'
+				'top': card.y + '%',
+				'left': card.x + '%'
 			});
-			if (window.innerHeight - mouseY < 395){
-				$('.card-settings').addClass('top');
+			if ($('.card').offset().top < 395){
+				$('.card-settings').removeClass('top').addClass('bottom');
 			} else {
-				$('.card-settings').addClass('bottom');
+				$('.card-settings').removeClass('bottom').addClass('top');
 			}
 			window.setTimeout(function() {
-				$('.card-settings').css('visibility','visible').addClass('fadeIn');
+				$('.card-settings').css('visibility','visible').removeClass('fadeOut').addClass('fadeIn');
+			}, 500);
+			$('.current-text').focus();
+		} else {
+			$scope.currentTime = $scope.player.getCurrentTime();
+			var offset = $(".player-container").offset();
+			currentX = ((mouseX - 32)/$(".player-container").width())*100;
+			currentY = ((mouseY - offset.top - 32)/$(".player-container").height())*100;
+			$('.card').removeClass('bounceOut').addClass('bounceIn');
+			$('.card').css({
+				'visibility': 'visible',
+				'top': currentY + '%',
+				'left': currentX + '%'
+			});
+			if (window.innerHeight - mouseY < 395){
+				$('.card-settings').removeClass('bottom').addClass('top');
+			} else {
+				$('.card-settings').removeClass('top').addClass('bottom');
+			}
+			window.setTimeout(function() {
+				$('.card-settings').css('visibility','visible').removeClass('fadeOut').addClass('fadeIn');
 			}, 500);
 			$('.current-text').focus();
 			cardOpen = true;
-		} else {
-			//	if (cardOpen){
-			//		$('.card').removeClass('bounceIn').addClass('bounceOut');
-			//		$scope.collapseSidebar();
-			//	cardOpen = false;
-			//	}
-			//	$scope.player.playVideo();
 		}
 	};
 
@@ -155,6 +160,60 @@ function MainController($scope, $window) {
 
 	$scope.colorSelect = function(color){
 		$scope.emojiBgColor = color;
+	};
+
+	$scope.closeCard = function(){
+		$('.card-settings').removeClass('fadeIn').addClass('fadeOut');
+		window.setTimeout(function() {
+			$('.card').removeClass('bounceIn').addClass('bounceOut');
+			$('.card-settings').css('visibility','hidden');
+			cardOpen = false;
+			$scope.player.playVideo();
+			window.setTimeout(function() {
+				$scope.currentText = '';
+				$scope.selectedEmoji = ':wolf:';
+				$scope.emojiBgColor = '#7bd148';
+			}, 500);
+		}, 500);
+	};
+
+	$scope.saveCard = function() {
+		var cardData = {
+			'time': $scope.currentTime,
+			'text': $scope.currentText,
+			'emoji': $scope.selectedEmoji,
+			'bg': $scope.emojiBgColor,
+			'x': currentX,
+			'y': currentY
+		};
+		if ($scope.cardIndex.length === 0) {
+			$scope.cardIndex[0] = cardData;
+			$scope.closeCard();
+		} else {
+			for (var i = 0; i < $scope.cardIndex.length; i++){
+				if ($scope.cardIndex[i].time === $scope.currentTime){
+					$scope.cardIndex[i] = cardData;
+					break;
+				} else if (i+1 === $scope.cardIndex.length) {
+					$scope.cardIndex.push(cardData);
+					break;
+				} else if ($scope.cardIndex[i].time > $scope.currentTime) {
+					$scope.cardIndex.splice(i, 0, cardData);
+					break;
+				}
+			}
+			$scope.closeCard();
+		}
+	};
+
+	$scope.deleteCard = function() {
+		for (var i = 0; i < $scope.cardIndex.length; i++){
+			if ($scope.cardIndex[i].time === $scope.currentTime){
+				$scope.cardIndex.splice(i,1);
+				break;
+			}
+		}
+		$scope.closeCard();
 	};
 }
 
