181ff1495c3f9220f34ccadbb96d92cb41b431bf
Jeremy Laskar <jlaskar@disqus.com>
2016-01-06 13:58:57 -0800

    mongo wip

---
+++ app/models/entry.js
@@ -0,0 +1,4 @@
+var mongoose = require('mongoose');
+module.exports = mongoose.model('Entry', {
+    slug: {type : String}
+});
--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,40 @@
+var Entry = require('./models/entry');
+var shortId = require('shortid');
+
 module.exports = function(app) {
 
 	// server routes
 	// ===========================================================
 	// handle things like api calls
 	// authentication routes
+
+	app.get('/api/test', function(req, res) {
+        // use mongoose to get all nerds in the database
+        Entry.find(function(err, entries) {
+
+            // if there is an error retrieving, send the error.
+            // nothing after res.send(err) will execute
+            if (err)
+                res.send(err);
+
+            res.json(entries); // return all nerds in JSON format
+        });
+    });
+
+    app.get('/api/testsave', function(req, res){
+
+    	var slug = shortId.generate();
+
+    	var test = new Entry({
+    		slug: slug
+    	});
+
+    	test.save(function(err){
+    		if (err) res.send(err);
+    		res.send('saved. id: ' + slug);
+    	});
+
+    });
 
 	// frontend routes
 	// =========================================================
