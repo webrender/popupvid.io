fd983f397cde8deed49f9c1f962ffe22364aa94e
Jeremy Laskar <webrender@gmail.com>
2016-03-14 16:22:37 -0700

    body.modal-open.length

--- public/src/js/controllers/EditCtrl.js
+++ public/src/js/controllers/EditCtrl.js
@@ -415,11 +415,13 @@ function EditController($scope, $window, $document, $timeout, $http, $stateParam
 	};
 
 	$document.on('keypress', function(event){
-		if ($scope.player && !cardOpen && !$(".form-title").is(':focus') && !$("body.modal-open")){
-			if ($scope.state == 1)
+		if ($scope.player && !cardOpen && !$(".form-title").is(':focus') && !$("body.modal-open").length){
+			if ($scope.state == 1){
+				console.log('pauseVideo');
 				$scope.sendMessage({'event': 'pauseVideo'});
-			else
+			} else {
 				$scope.sendMessage({'event': 'playVideo'});
+			}
 		}
 	});
 
